133ef0d40e6fd2e3ce222158740873354296dc57
Thien Vo <vohuythien@utexas.edu>
2019-03-11 01:02:15 -0500

    Decided to export NODE_ENV before running project instead

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ Then put in the Lease Name and Well Num and the bot will spit out a map with ins
 * Requires for you to have a Google Cloud Platform account with billing enabled
 * ngrok is required for Twilio to work. Please tunnel the project using something like "ngrok http 3000"
 * I have included the credentials and environment variables in this case to be able to run locally without a hitch.
-* To run the project locally, just do "npm startWatch" (This uses the dotenv module to import the environment variables)
-
-
+* To run the project locally:
+    * export NODE_ENV=development
+    * run "npm startWatch"
 
 ### Prerequisites
 
--- db/FBAdmin.js
+++ db/FBAdmin.js
@@ -1,20 +1,12 @@
 const admin = require('firebase-admin');
 const path = require('path');
-
 let serviceAccount = null;
+
 if (process.env.NODE_ENV === 'development') {
     serviceAccount = require(path.join(__dirname, '..', process.env.FIREBASE_APPLICATION_CREDENTIALS));
 } else {
-    serviceAccount = require(process.env.FIREBASE_APPLICATION_CREDENTIALS);
+    serviceAccount = process.env.FIREBASE_APPLICATION_CREDENTIALS;
 }
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     databaseURL: process.env.FIREBASE_DB_URL
-// });
-
-
-// const db = admin.database();
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)

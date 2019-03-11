fc1e1d9ba244c37f698794e7aee9fa408ce702c4
Thien Vo <vohuythien@utexas.edu>
2019-03-11 00:42:53 -0500

    Changed fbadmin to use only environment variable on production

--- db/FBAdmin.js
+++ db/FBAdmin.js
@@ -1,6 +1,12 @@
 const admin = require('firebase-admin');
 const path = require('path');
-const serviceAccount = require(path.join(__dirname, '..', process.env.FIREBASE_APPLICATION_CREDENTIALS));
+
+let serviceAccount = null;
+if (process.env.NODE_ENV === 'development') {
+    serviceAccount = require(path.join(__dirname, '..', process.env.FIREBASE_APPLICATION_CREDENTIALS));
+} else {
+    serviceAccount = require(process.env.FIREBASE_APPLICATION_CREDENTIALS);
+}
 
 // admin.initializeApp({
 //     credential: admin.credential.cert(serviceAccount),

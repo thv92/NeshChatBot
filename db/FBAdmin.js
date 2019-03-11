d62f0017e00d536826905ba9881a4dcd7ff9063e
Thien Vo <vohuythien@utexas.edu>
2019-03-11 01:07:31 -0500

    Getting ERRNAMETOOLONG error. Trying to parse as json instead

--- db/FBAdmin.js
+++ db/FBAdmin.js
@@ -5,7 +5,7 @@ let serviceAccount = null;
 if (process.env.NODE_ENV === 'development') {
     serviceAccount = require(path.join(__dirname, '..', process.env.FIREBASE_APPLICATION_CREDENTIALS));
 } else {
-    serviceAccount = process.env.FIREBASE_APPLICATION_CREDENTIALS;
+    serviceAccount = JSON.parse(process.env.FIREBASE_APPLICATION_CREDENTIALS);
 }
 
 admin.initializeApp({
